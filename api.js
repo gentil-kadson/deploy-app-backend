dd69a2bb35a80278b3026dc1333c9d418cd8b738
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-07-10 21:54:12 -0300

    post request implemented.

--- api.js
+++ api.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Deploy application running on port ${port}`);
 });
 
 app.get("/", (req, res) => res.json("Oi, eu sou o Goku!"));
+
+app.post("/upload-file", (req, res) => {
+  res.send(req.body.arquivo);
+});
