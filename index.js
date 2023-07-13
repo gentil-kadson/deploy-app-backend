1c3e366c39ff4983183daf6f399ad2986b7859a8
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-07-13 16:57:25 -0300

    removed front-end files as they will be used as another application.

--- index.js
+++
@@ -1,9 +0,0 @@
-const fileForm = document.getElementById("file-form");
-const fileUpload = document.querySelector("[name='arquivo']");
-const filenameSpan = document.getElementById("filename");
-
-function handleUploadedFileName() {
-  let formattedFileName = fileUpload.value.split("\\");
-  formattedFileName = formattedFileName[formattedFileName.length - 1];
-  filenameSpan.innerText = formattedFileName;
-}
