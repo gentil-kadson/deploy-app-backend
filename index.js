f15f38771e02652ae1eff6842bd424b940bea09d
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-07-15 18:37:21 -0300

    set up for aws s3 bucket.

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 // Imports
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Configuring dotenv lib
+dotenv.config();
+
+// Configuring AWS lib
+import aws from "aws-sdk";
+import multer from "multer";
+import multerS3 from "multer-s3";
+
+aws.config.update({
+  secretAccessKey: process.env.ACCESS_SECRET,
+  accessKeyId: process.env.ACCESS_KEY,
+  region: process.env.REGION,
+});
+const BUCKET = process.env.BUCKET;
+const s3 = new aws.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: BUCKET,
+    key: function (req, file, cb) {
+      console.log(file);
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 // Configuring express
 const app = express();
@@ -20,9 +49,7 @@ const imagesSchema = new mongoose.Schema({
 });
 
 // Connecting to DB and instanciating images model
-mongoose.connect(
-  "mongodb+srv://kadson:a7x@cluster0.mmdhgv2.mongodb.net/?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGODB_URL);
 const db = mongoose.connection;
 const Images = db.model("Image", imagesSchema);
 
@@ -30,10 +57,11 @@ const Images = db.model("Image", imagesSchema);
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
 
-// Using the express API
+// Configuring body-parser
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/images", async (req, res) => {
+app.post("/api/upload", upload.single("imageUrl"), async (req, res) => {
   const image = new Images({
     title: req.body.title,
     imageUrl: req.body.imageUrl,
@@ -47,10 +75,15 @@ app.post("/images", async (req, res) => {
   }
 });
 
-app.get("/images", async (req, res) => {
+app.get("/api/images", async (req, res) => {
   try {
+    // Get image files from s3 bucket
+    const response = await s3.listObjectsV2({ Bucket: BUCKET }).promise();
+    const imageFiles = response.Contents.map((item) => item.Key);
+
+    // Sending objects to user
     const images = await Images.find();
-    res.json(images);
+    res.json({ bdObjects: images, files: imageFiles });
   } catch (err) {
     res.status(500).json({ message: err });
   }
