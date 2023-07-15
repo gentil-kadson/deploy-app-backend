afeb7d9f4d45b7936013bf4793338a4233b6a49a
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-07-15 12:16:05 -0300

    finished node API

--- index.js
+++ index.js
@@ -30,10 +30,30 @@ const Images = db.model("Image", imagesSchema);
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
 
+// Using the express API
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json("Oi Beto");
+app.post("/images", async (req, res) => {
+  const image = new Images({
+    title: req.body.title,
+    imageUrl: req.body.imageUrl,
+  });
+
+  try {
+    const newImageObj = await image.save();
+    res.status(201).json(newImageObj);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+});
+
+app.get("/images", async (req, res) => {
+  try {
+    const images = await Images.find();
+    res.json(images);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
 
 app.listen(port, () => {
