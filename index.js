5ddfaac82b5f90a462892d0fb126f6632ee38614
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-07-15 12:09:34 -0300

    reintroduced the db.

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+// Imports
 import express from "express";
+import mongoose from "mongoose";
+
+// Configuring express
 const app = express();
 const port = 3000;
+
+// Configuring mongoose
+// Creating images Schema
+const imagesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+// Connecting to DB and instanciating images model
+mongoose.connect(
+  "mongodb+srv://kadson:a7x@cluster0.mmdhgv2.mongodb.net/?retryWrites=true&w=majority"
+);
+const db = mongoose.connection;
+const Images = db.model("Image", imagesSchema);
+
+// Seeing if there's any error on connection
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
 app.use(express.json());
 
